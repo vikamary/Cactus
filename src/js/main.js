f45bb9f7cabc778f574edc416c0a0a03803346b8
vikamarey <vikamarey@gmail.com>
2024-10-03 18:20:23 +0300

    removeMob

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,11 @@ if(navClose){
     navMenu.classList.remove('show-menu')
   })
 }
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction)) 
